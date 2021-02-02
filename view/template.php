895ba0992d157b38e709ca4a76e7db806ce459b8
ESrebuff <estebanianis@gmail.com>
2021-02-02 12:00:04 +0100

    initialisation des dossiers pour la structure mvc

---
+++ view/template.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <title><?= $title ?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">>
+    <script>
+        tinymce.init({
+            selector: 'textarea'
+        });
+
+    </script>
+</head>
+
+<body>
+    <?= $content ?>
+</body>
+
+</html>
